259cfe68ba4d8fad1062b72722bc4f982b262e74
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-09 22:53:26 +0200

    set up project rotes for adding and getting all projects!

---
+++ backend/app/controllers/project_routes.py
@@ -0,0 +1,14 @@
+from flask import Blueprint, request, jsonify
+from ..services.project_services import get_all_projects, create_new_project
+
+project_bp = Blueprint("projects", __name__)
+
+@project_bp.route("/", methods=["GET"])
+def get_projects():
+    return jsonify(get_all_projects())
+
+@project_bp.route("/", methods=["POST"])
+def create_project():
+    data = request.get_json()
+    project = create_new_project(data)
+    return jsonify(project), 201
