eaa37739e691ddfbba3db660ac462e127af239c5
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 01:51:28 +0200

    hyper basic frontend

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import "./homePage.css"
+
+export default function HomePage() {
+    console.log("test")
+
+    return (
+        <section className="homePageSection">
+            <div className="homeContainer">
+                <p>Home Page WOW!</p>
+            </div>
+        </section>
+    )
+}
