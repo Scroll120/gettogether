2641dff1b2c35b9d154d4058842b64fd8b2c2eeb
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 00:39:19 +0200

    Project model added user relation

--- backend/app/models/project.py
+++ backend/app/models/project.py
@@ -4,3 +4,6 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(100), nullable = False)
     description = db.Column(db.Text, nullable = False)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='projects')
