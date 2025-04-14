3b36edb62023c29fc396ba3a71087db71d9737ff
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-14 12:10:04 +0200

    user service save

--- backend/app/controllers/user_routes.py
+++ backend/app/controllers/user_routes.py
@@ -6,3 +6,7 @@ user_bp = Blueprint("user", __name__)
 @user_bp.route("/", methods=["GET"])
 def get_users():
     return jsonify(get_all_users())
+
+@user_bp.route("/", methods=["POST"])
+def create_user():
+    return jsonify(create_user())
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -4,3 +4,8 @@ from ..models.user import User
 def get_all_users():
     users = get_all()
     return [{"id": u.id, "username": u.username} for u in users]
+
+def create_user(data):
+    user = User(username=data["username"])
+    save(user)
+    return {"id": user.id, "username": user.username}
