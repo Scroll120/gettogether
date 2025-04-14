8b63debbdb438549c2c7125836d58048c79cfbc4
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 00:59:12 +0200

    fixed import regarding associations.py

--- backend/app/models/associations.py
+++ backend/app/models/associations.py
@@ -1,6 +1,6 @@
 from .. import db
 
-project_users = db.Table('project_users', 
+project_accounts = db.Table('project_users', 
         db.Column('account_id', db.Integer, db.ForeignKey('account.id'), primary_key=True),
         db.Column('project_id', db.Integer, db.ForeignKey('project.id'), primary_key=True)
 )
