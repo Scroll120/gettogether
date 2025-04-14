09c5c888a1e885a4d2610c4c9cd0a3fbe87a623c
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 00:40:25 +0200

    User added project relation, and password field

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -3,3 +3,6 @@ from .. import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), nullable = False)
+    password = db.Column(db.String(100), nullable = False)
+
+    projects = db.relationship('Project', back_populates = 'user', cascade = "all, delete-orphan")
