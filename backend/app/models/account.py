921b61a51f0f3d791350b8da5a2234714fa526b1
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 03:16:22 +0200

    functioning register method

--- backend/app/models/account.py
+++ backend/app/models/account.py
@@ -4,7 +4,7 @@ from .associations import project_accounts
 class Account(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(100), unique=True, nullable = False)
-    email = db.Column(db.string(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(256), nullable = False)
 
     projects = db.relationship('Project', secondary=project_accounts, back_populates='accounts')
