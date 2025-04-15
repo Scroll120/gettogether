eea2b677288bd891c9f87630fc741c7b223eecc2
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 04:00:01 +0200

    added get by id

--- backend/app/services/account_service.py
+++ backend/app/services/account_service.py
@@ -1,9 +1,17 @@
-from ..repositories.account_repository import get_all, save
+from ..repositories.account_repository import get_all, save, get_by_id
 from ..models.account import Account
 
 def get_all_accounts():
     accounts = get_all()
     return [{"id": a.id, "username": a.username} for a in accounts]
+
+def get_account_by_id(id):
+    account = get_by_id(id)
+    return {"id": account.id,
+             "username": account.username,
+             "email": account.email
+             }
+    return None
 
 def create_new_account(data):
     account = Account(username=data["username"])
