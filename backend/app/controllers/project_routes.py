3d923867fb56267c872b2e0904653574d4d9056f
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-15 03:53:33 +0200

    added token requried for project get all and create project

--- backend/app/controllers/project_routes.py
+++ backend/app/controllers/project_routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, request, jsonify
 from ..services.project_service import get_all_projects, create_new_project
+from ..utils.auth_utils import token_required
 
 project_bp = Blueprint("projects", __name__)
 
 @project_bp.route("/", methods=["GET"])
+@token_required
 def get_projects():
-    return jsonify(get_all_projects())
+    return jsonify(get_all_projects()), 200
 
 @project_bp.route("/", methods=["POST"])
+@token_required
 def create_project():
     data = request.get_json()
     project = create_new_project(data)
