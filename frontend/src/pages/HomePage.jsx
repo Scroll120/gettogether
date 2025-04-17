75c962a1f1aa4d1c498d694ffb9e0f6ef5b64d68
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-17 15:29:46 +0200

    hangout carousel at the bottom of the screen added, timing adjusted

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,6 +14,26 @@ export default function HomePage() {
                     <img src={logo} alt="Logo" className="home-logo"/>
                     <h1>Get Together</h1>
                 </div>
+                <div className="home-page-project-slide-container">
+                    <div className="carousel">
+                        <div className="carousel-track">
+                            <div className="project-card">1</div>
+                            <div className="project-card">2</div>
+                            <div className="project-card">3</div>
+                            <div className="project-card">4</div>
+                            <div className="project-card">5</div>
+                            <div className="project-card">6</div>
+                            <div className="project-card">7</div>
+                            {/* Repeat 1-3 to make it seamless */}
+                            <div className="project-card">1</div>
+                            <div className="project-card">2</div>
+                            <div className="project-card">3</div>
+                            <div className="project-card">4</div>
+                            <div className="project-card">5</div>
+                            <div className="project-card">6</div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
