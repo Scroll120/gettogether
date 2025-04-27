df22a941171e495cbbe5eda5e9d1dde5c6d70f99
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-27 16:23:11 +0200

    login input field with handlesubmit function added

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -2,6 +2,7 @@ import './loginPage.css';
 import bgvideo from '../../assets/video-bg.mov'
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
+import InputField from '../../components/InputField/InputField';
 
 export default function LoginPage() {
     const navigate = useNavigate()
@@ -14,7 +15,7 @@ export default function LoginPage() {
     // http://localhost:5000/api/auth/login
     async function handleSubmit(e) {
         e.preventDefault();
-        console.log("aa")
+        console.log(loginDetails)
     }
 
 
@@ -27,8 +28,30 @@ export default function LoginPage() {
                 <div className='login-box'>
                     <form onSubmit={handleSubmit} className='login-form'>
                         <div className='email-input-box'>
+                            <InputField
+                            className='email-input-field'
+                            placeHolder='Email'
+                            value={loginDetails.email}
+                            onChange={(e) => {setLoginDetails(
+                                {
+                                    email: e.target.value,
+                                    password: loginDetails.password
+                                }
+                            )}}
+                            />
                         </div>
                         <div className='password-input-box'>
+                            <InputField
+                            className='password-input-field'
+                            placeHolder='Password'
+                            value={loginDetails.password}
+                            onChange={(e) => {setLoginDetails(
+                                {
+                                    email: loginDetails.email,
+                                    password: e.target.value
+                                }
+                            )}}
+                            />
                         </div>
                         <button className='signin-button'>Sign In</button>
                         <button onClick={ () => { navigate('/register')}} className='signup-button'>Sign Up</button>
