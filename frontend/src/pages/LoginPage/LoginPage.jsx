53419919dc1e3d196b248e6932f21de3fdd3f95b
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-27 16:14:26 +0200

    login box structure, no css implemented

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,9 +1,40 @@
 import './loginPage.css';
+import bgvideo from '../../assets/video-bg.mov'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function LoginPage() {
+    const navigate = useNavigate()
+    const [loginDetails, setLoginDetails] = useState({
+        email: '',
+        password: '',
+    });
+
+
+    // http://localhost:5000/api/auth/login
+    async function handleSubmit(e) {
+        e.preventDefault();
+        console.log("aa")
+    }
+
+
 
     return (
-        <>
-        </>
+        <section className='login-page-section'>
+            <div className='login-container'>
+                <video src={bgvideo} loop muted autoPlay />
+                <div className='login-overlay'></div>
+                <div className='login-box'>
+                    <form onSubmit={handleSubmit} className='login-form'>
+                        <div className='email-input-box'>
+                        </div>
+                        <div className='password-input-box'>
+                        </div>
+                        <button className='signin-button'>Sign In</button>
+                        <button onClick={ () => { navigate('/register')}} className='signup-button'>Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </section>
     )
 }
