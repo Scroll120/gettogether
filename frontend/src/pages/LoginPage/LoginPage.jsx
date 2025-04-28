328a7d846ae7eca41c09bd1f2e76312945ea38a2
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-28 06:16:39 +0200

    login page css and login handling

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -10,6 +10,7 @@ export default function LoginPage() {
         username: '',
         password: '',
     });
+    const [isFailedLogin, setIsFailedLogin] = useState(false);
 
 
     // http://localhost:5000/api/auth/login
@@ -25,14 +26,12 @@ export default function LoginPage() {
         })
 
         if (response.status === 401) {
-            console.log("merp")
+            setIsFailedLogin(true)
         } else if (response.status === 200 ) {
             const data = await response.json();
             localStorage.setItem("user", JSON.stringify(data));
-            console.log(data)
+            navigate('/')
         }
-        
-        console.log(loginDetails)
     }
 
 
@@ -43,12 +42,20 @@ export default function LoginPage() {
                 <video src={bgvideo} loop muted autoPlay />
                 <div className='login-overlay'></div>
                 <div className='login-box'>
+                    { isFailedLogin ?
+                            <div className='failed-login-message-box'>
+                                <p className='failed-login-message'>Incorrect Credentials!</p>
+                            </div>
+                        :
+                            <></>
+                    }
                     <form onSubmit={handleSubmit} className='login-form'>
                         <div className='username-input-box'>
                             Username
                             <InputField
                             className='username-input-field'
                             placeHolder='Username'
+                            type='text'
                             value={loginDetails.username}
                             onChange={(e) => {setLoginDetails(
                                 {...loginDetails, username: e.target.value}
@@ -60,6 +67,7 @@ export default function LoginPage() {
                             <InputField
                             className='password-input-field'
                             placeHolder='Password'
+                            type='password'
                             value={loginDetails.password}
                             onChange={(e) => {setLoginDetails(
                                 {...loginDetails, password: e.target.value}
@@ -67,7 +75,6 @@ export default function LoginPage() {
                             />
                         </div>
                         <button className='signin-button'>Sign In</button>
-                        <button onClick={ () => { navigate('/register')}} className='signup-button'>Sign Up</button>
                     </form>
                 </div>
             </div>
