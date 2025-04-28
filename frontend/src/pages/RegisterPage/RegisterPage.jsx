94d15d9f8ec310e0e87375ec686aad7c4a657439
KocsisAGyorgy <gurikocsis31@gmail.com>
2025-04-28 07:27:31 +0200

    register page made, backend frontend communication

--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,9 +1,96 @@
 import './registerPage.css';
+import bgvideo from '../../assets/video-bg.mov'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import InputField from '../../components/InputField/InputField';
 
 export default function RegisterPage() {
+    const navigate = useNavigate()
+    const [registerDetails, setRegisterDetails] = useState({
+        username: '',
+        password: '',
+        emial: '',
+    });
+    const [isFailedRegister, setIsFailedRegister] = useState(false);
+
+
+        // http://localhost:5000/api/auth/register
+    async function handleSubmit(e) {
+        e.preventDefault();
+
+        const response = await fetch("http://localhost:5000/api/auth/register", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({username: registerDetails.username, password: registerDetails.password, email: registerDetails.email})
+        })
+
+        if (response.status === 401) {
+            setIsFailedRegister(true)
+        } else if (response.status === 200 ) {
+            const data = await response.json();
+            console.log(data)
+            navigate('/')
+        }
+    }
+
+
 
     return (
-        <>
-        </>
+        <section className='register-page-section'>
+            <div className='register-container'>
+                <video src={bgvideo} loop muted autoPlay />
+                <div className='register-overlay'></div>
+                <div className='register-box'>
+                    { isFailedRegister ?
+                            <div className='failed-register-message-box'>
+                                <p className='failed-register-message'>Failed to Register!</p>
+                            </div>
+                        :
+                            <></>
+                    }
+                    <form onSubmit={handleSubmit} className='register-form'>
+                        <div className='username-input-box'>
+                            Username
+                            <InputField
+                            className='username-input-field'
+                            placeHolder='Username'
+                            type='text'
+                            value={registerDetails.username}
+                            onChange={(e) => {setRegisterDetails(
+                                {...registerDetails, username: e.target.value}
+                            )}}
+                            />
+                        </div>
+                        <div className='email-input-box'>
+                            Email
+                            <InputField
+                            className='email-input-field'
+                            placeHolder='Email'
+                            type='text'
+                            value={registerDetails.email}
+                            onChange={(e) => {setRegisterDetails(
+                                {...registerDetails, email: e.target.value}
+                            )}}
+                            />
+                        </div>
+                        <div className='password-input-box'>
+                            Password
+                            <InputField
+                            className='password-input-field'
+                            placeHolder='Password'
+                            type='password'
+                            value={registerDetails.password}
+                            onChange={(e) => {setRegisterDetails(
+                                {...registerDetails, password: e.target.value}
+                            )}}
+                            />
+                        </div>
+                        <button className='signup-button'>Sign Up</button>
+                    </form>
+                </div>
+            </div>
+        </section>
     )
 }
